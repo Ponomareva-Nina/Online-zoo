56ab41ffbbc34101264b45d0a1811deb8216ea64
Nina Ponomareva <nina.9412@mail.ru>
2022-10-09 15:59:53 +0300

    feat: add burger menu behavior on page donate

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -1,3 +1,10 @@
 import '../../assets/styles/normalize.css';
 import '../../assets/styles/donate.scss';
 import '../../assets/styles/donate-adaptive.scss';
+import toggleMenu from '../../modules/menu';
+
+// === BURGER MENU ===
+const BurgerBtn = document.querySelector('.burger-menu');
+const Menu = document.querySelector('.main-nav');
+BurgerBtn.addEventListener('click', () => toggleMenu(BurgerBtn, Menu));
+// === /BURGER MENU ===
