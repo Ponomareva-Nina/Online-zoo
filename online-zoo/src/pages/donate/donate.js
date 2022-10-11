3f1487e8698c69d4ab256bb3b41aa186248db8ee
Nina Ponomareva <nina.9412@mail.ru>
2022-10-11 19:04:31 +0300

    feat: add basic donation form behavior

--- online-zoo/src/pages/donate/donate.js
+++ online-zoo/src/pages/donate/donate.js
@@ -8,3 +8,39 @@ const BurgerBtn = document.querySelector('.burger-menu');
 const Menu = document.querySelector('.main-nav');
 BurgerBtn.addEventListener('click', () => toggleMenu(BurgerBtn, Menu));
 // === /BURGER MENU ===
+
+// === AMOUNT BAR ===
+const AmountInput = document.querySelector('#amount');
+const AmountBar = document.querySelectorAll('.amount-bar__radio-input');
+
+function limitInput() {
+  if (this.value.length >= 5) {
+    this.value = this.value.substring(0, 4);
+  }
+  if (this.value.includes('e')) {
+    this.value.replace(/[e]/, '');
+  }
+}
+
+function changeAmountInput(amountBarValue, amountInput) {
+  // eslint-disable-next-line no-param-reassign
+  amountInput.value = amountBarValue.value;
+}
+
+function changeAmountBar(amountBar, amountInput) {
+  amountBar.forEach((el) => {
+    if (el.value === amountInput.value) {
+      // eslint-disable-next-line no-param-reassign
+      el.checked = true;
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      el.checked = false;
+    }
+  });
+}
+
+AmountInput.addEventListener('input', limitInput);
+AmountInput.addEventListener('input', () => { changeAmountBar(AmountBar, AmountInput); });
+AmountBar.forEach((el) => el.addEventListener('click', () => { changeAmountInput(el, AmountInput); }));
+
+// === AMOUNT BAR ===
