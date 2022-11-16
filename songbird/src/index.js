137fe1f149d5fb6aadcbccdb4a2fa1edc8e6c912
Nina Ponomareva <nina.9412@mail.ru>
2022-11-16 18:09:14 +0300

    feat: sreate start-page

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -2,8 +2,14 @@ import './assets/styles/main.scss';
 import createHeader from './scripts/components/header';
 import logo from './assets/images/logo.svg';
 import createFooter from './scripts/components/footer';
+import createStartPage from './scripts/components/start-page';
+import createElem from './scripts/utils/create-element';
 
 const root = document.getElementById('root');
 const header = createHeader(logo, 'Songbird', ['Главная', 'Игра', 'Справочник']);
 const footer = createFooter();
-root.append(header, footer);
+const main = createElem('main', 'wrapper main');
+const startPage = createStartPage();
+
+main.append(startPage);
+root.append(header, main, footer);
---
+++ songbird/src/scripts/components/start-page.js
@@ -0,0 +1,27 @@
+import createElem from '../utils/create-element';
+
+const birds = [
+  { src: './assets/images/vulture.png', text: 'hiss-hiss' },
+  { src: './assets/images/owl.png', text: 'whoo-whoo' },
+  { src: './assets/images/sparrow.png', text: 'tweet' },
+  { src: './assets/images/heron.png', text: 'ro-ro-ro' }];
+
+export default function createStartPage() {
+  const container = createElem('div', 'start-page');
+  const title = createElem('h2', 'intro-title', 'Songbird - музыкально-дидактическая викторина для распознавания птиц по их голосам.');
+  const btn = createElem('button', 'btn start-game-btn', 'Играть');
+  const birdsList = createElem('ul', 'birds-intro');
+
+  for (let i = 0; i < birds.length; i++) {
+    const bird = createElem('li', 'birds-intro__bird');
+    const img = createElem('img');
+    img.setAttribute('src', birds[i].src);
+    img.setAttribute('alt', '');
+    const text = createElem('p', '', birds[i].text);
+    bird.append(img, text);
+    birdsList.append(bird);
+  }
+
+  container.append(title, btn, birdsList);
+  return container;
+}
