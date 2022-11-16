0d91a77ab41749c1d49144072f367aa14bfb09f2
Nina Ponomareva <nina.9412@mail.ru>
2022-11-16 20:40:44 +0300

    refactor: refactor createHeader function

--- songbird/src/scripts/components/header.js
+++ songbird/src/scripts/components/header.js
@@ -1,24 +1,31 @@
 import createElem from '../utils/create-element';
+import logo from '../../assets/images/logo.svg';
 
-export default function createHeader(logoImg, logoName, navigationLinks) {
+const navigationLinks = [
+  { name: 'Главная', href: '#start' },
+  { name: 'Игра', href: '#game' },
+  { name: 'Справочник', href: '#catalogue' }];
+
+export default function createHeader() {
   const header = createElem('header', 'header');
-  const logo = createElem('div', 'logo');
+  const logoContainer = createElem('div', 'logo');
   const img = createElem('img');
-  img.setAttribute('src', logoImg);
+  img.setAttribute('src', logo);
   img.setAttribute('alt', '');
-  const title = createElem('h1', '', logoName);
-  logo.append(img, title);
+  const title = createElem('h1', '', 'Songbird');
+  logoContainer.append(img, title);
   const nav = createElem('ul', 'main-nav');
 
   for (let i = 0; i < navigationLinks.length; i++) {
     const li = createElem('li');
-    const a = createElem('a', '', navigationLinks[i]);
-    a.setAttribute('href', '#');
+    const a = createElem('a', '', navigationLinks[i].name);
+    a.setAttribute('href', navigationLinks[i].href);
+
     if (i === 0) { a.className = 'active-link'; }
     li.append(a);
     nav.append(li);
   }
 
-  header.append(logo, nav);
+  header.append(logoContainer, nav);
   return header;
 }
