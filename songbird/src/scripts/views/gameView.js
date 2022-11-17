8927654a6612f3cb2e7f11f5d3d14c0b96187daf
Nina Ponomareva <nina.9412@mail.ru>
2022-11-18 00:08:19 +0300

    feat: update game view

--- songbird/src/scripts/views/gameView.js
+++ songbird/src/scripts/views/gameView.js
@@ -6,9 +6,10 @@ export default class GameView {
     this.controller = new GameController();
     this.AudioPlayerView;
     this.scoreContainer = createElem('div', 'score');
-    this.nextBtn = createElem('button', 'btn btn-next');
+    this.nextBtn = createElem('button', 'btn btn-next btn_disabled', 'Далее');
     this.birdName = '******';
     this.birdImg = './assets/images/question-bird.png';
+    this.answers = [];
   }
 
   updateScore() {
@@ -33,9 +34,12 @@ export default class GameView {
     this.nextBtn.classListRemove('btn_disabled');
   }
 
-  createGamePage() {
-    const gameContainer = createElem('section', 'game wrapper');
-    const categories = this.createCategories();
+  createPlayer() {
+    const playerContainer = createElem('div', 'question__player');
+    return playerContainer;
+  }
+
+  createQuestionContainer() {
     const questionContainer = createElem('div', 'question');
     const birdImgContainer = createElem('div', 'question__bird-img');
     const birdImg = createElem('img');
@@ -43,10 +47,37 @@ export default class GameView {
     birdImg.setAttribute('src', this.birdImg);
     birdImgContainer.append(birdImg);
     const birdName = createElem('div', 'question__bird-name', this.birdName);
-    const playerContainer = createElem('div', 'question__player');
-    questionContainer.append(birdImgContainer, birdName, playerContainer);
 
-    gameContainer.append(this.scoreContainer, categories, questionContainer);
+    const player = this.createPlayer();
+    questionContainer.append(birdImgContainer, birdName, player);
+    return questionContainer;
+  }
+
+  updateAnswers() {
+    const currentAnswers = this.controller.getAnswers();
+    currentAnswers.forEach((answer) => {
+      const li = createElem('li', 'answer', answer);
+      const circle = createElem('span', 'circle');
+      li.prepend(circle);
+      this.answers.push(li);
+    });
+  }
+
+  createGamePage() {
+    const gameContainer = createElem('section', 'game wrapper');
+    const categories = this.createCategories();
+    const question = this.createQuestionContainer();
+    const answersSection = createElem('div', 'answers');
+    const variants = createElem('ul', 'answers__list');
+    this.updateAnswers();
+    this.answers.forEach((answer) => {
+      variants.append(answer);
+    });
+
+    const birdCardContainer = createElem('div', 'answers__bird-info');
+    answersSection.append(variants, birdCardContainer);
+    gameContainer.append(this.scoreContainer, categories, question, answersSection, this.nextBtn);
+
     return gameContainer;
   }
 }
