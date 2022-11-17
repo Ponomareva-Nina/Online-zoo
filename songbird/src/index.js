6f8566735087788b4fab1fcd61962cbb32e5527f
Nina Ponomareva <nina.9412@mail.ru>
2022-11-17 23:09:27 +0300

    feat: complete navigation on website

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -1,26 +1,36 @@
-import './assets/styles/main.scss';
-import createHeader from './scripts/components/header';
+/* eslint-disable no-use-before-define */
+import './assets/styles/main-page.scss';
+import './assets/styles/game-page.scss';
+import Header from './scripts/components/header';
 import createFooter from './scripts/components/footer';
 import createStartPage from './scripts/components/start-page';
 import createElem from './scripts/utils/create-element';
 import { getHash, Router } from './scripts/router';
+import GameView from './scripts/views/gameView';
 
 const root = document.getElementById('root');
-const header = createHeader();
+const headerView = new Header();
+const header = headerView.renderHeader();
 const footer = createFooter();
 const main = createElem('main', 'wrapper main');
 const [startGameBtn, startPage] = createStartPage();
 startGameBtn.onclick = () => { window.location.hash = '#game'; };
-const gamePage = 'gamePage';
+const Game = new GameView();
+const gamePage = Game.createGamePage();
 const cataloguePage = 'cataloguePage';
 const Route = new Router(main, startPage, gamePage, cataloguePage);
 
 root.append(header, main, footer);
 
 window.addEventListener('hashchange', () => {
-  Route[`${getHash()}Route`]();
+  updateRoute();
 });
 
 window.addEventListener('load', () => {
-  Route[`${getHash()}Route`]();
+  updateRoute();
 });
+
+function updateRoute() {
+  Route[`${getHash()}Route`]();
+  headerView.updateActiveLink(document.querySelector(`a[href="#${getHash()}"]`));
+}
--- songbird/src/scripts/components/header.js
+++ songbird/src/scripts/components/header.js
@@ -1,31 +1,42 @@
 import createElem from '../utils/create-element';
 import logo from '../../assets/images/logo.svg';
 
-const navigationLinks = [
-  { name: 'Главная', href: '#start' },
-  { name: 'Игра', href: '#game' },
-  { name: 'Справочник', href: '#catalogue' }];
-
-export default function createHeader() {
-  const header = createElem('header', 'header');
-  const logoContainer = createElem('div', 'logo');
-  const img = createElem('img');
-  img.setAttribute('src', logo);
-  img.setAttribute('alt', '');
-  const title = createElem('h1', '', 'Songbird');
-  logoContainer.append(img, title);
-  const nav = createElem('ul', 'main-nav');
-
-  for (let i = 0; i < navigationLinks.length; i++) {
-    const li = createElem('li');
-    const a = createElem('a', '', navigationLinks[i].name);
-    a.setAttribute('href', navigationLinks[i].href);
-
-    if (i === 0) { a.className = 'active-link'; }
-    li.append(a);
-    nav.append(li);
+export default class Header {
+  constructor() {
+    this.routes = [
+      { name: 'Главная', href: '#start' },
+      { name: 'Игра', href: '#game' },
+      { name: 'Справочник', href: '#catalogue' },
+    ];
+    this.activeLink;
   }
 
-  header.append(logoContainer, nav);
-  return header;
+  updateActiveLink(a) {
+    if (this.activeLink) this.activeLink.classList.remove('active-link');
+    this.activeLink = a;
+    this.activeLink.classList.add('active-link');
+  }
+
+  renderHeader() {
+    const header = createElem('header', 'header');
+    const logoContainer = createElem('div', 'logo');
+    const img = createElem('img');
+    img.setAttribute('src', logo);
+    img.setAttribute('alt', '');
+    const title = createElem('h1', '', 'Songbird');
+    logoContainer.append(img, title);
+    const nav = createElem('ul', 'main-nav');
+
+    this.routes.forEach((route) => {
+      const li = createElem('li');
+      const a = createElem('a', '', route.name);
+      a.setAttribute('href', route.href);
+      a.addEventListener('click', () => { this.updateActiveLink(a); });
+      li.append(a);
+      nav.append(li);
+    });
+
+    header.append(logoContainer, nav);
+    return header;
+  }
 }
