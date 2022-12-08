e58630d688ba6a8c0affe4add0c7f3a3040369be
Nina Ponomareva <nina.9412@mail.ru>
2022-12-08 18:42:19 +0300

    feat: migrate app view to TS

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,22 @@
+import { NewsInterface, SourcesInterface } from '../../types/interfaces';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: NewsInterface) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: SourcesInterface) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
