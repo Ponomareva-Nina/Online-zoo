93af6cd182e0c99e8dbd25c1de44efe1c48a734f
Nina Ponomareva <nina.9412@mail.ru>
2022-12-08 21:57:20 +0300

    feat: migrate controller and appLoader class to TS

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -1,27 +1,29 @@
+import { NewsItem, SourceItem } from '../../types/interfaces';
+import { Endpoints } from '../../types/types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: (arg?: SourceItem[]) => void) {
         super.getResp(
             {
-                endpoint: 'sources',
+                endpoint: Endpoints.sources,
             },
             callback
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: (arg?: NewsItem[]) => void) {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
-                            endpoint: 'everything',
+                            endpoint: Endpoints.everything,
                             options: {
                                 sources: sourceId,
                             },
@@ -31,7 +33,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
