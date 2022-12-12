9c9cfe75407981c761c1427523b38215394afb0c
Nina Ponomareva <nina.9412@mail.ru>
2022-12-12 21:16:46 +0300

    feat: add custom scrollbar for sources

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,7 +1,30 @@
 import './sources.css';
 import { SourceItem } from '../../../types/interfaces';
+import { NullableElement } from '../../../types/types';
 
 class Sources {
+    scrollbar: NullableElement<HTMLInputElement>;
+
+    constructor() {
+        this.scrollbar = document.querySelector('.scrollbar');
+    }
+
+    private slideSources(SourcesNumber: number) {
+        const sourcesTrack = document.querySelector('.sources') as HTMLElement;
+        const TranslateValue = (sourcesTrack.scrollWidth - sourcesTrack.clientWidth) / SourcesNumber;
+        this.scrollbar?.setAttribute('max', SourcesNumber.toString());
+
+        if (this.scrollbar) {
+            let currentTranslate = `translateX(-${Number(this.scrollbar.value) * TranslateValue}px)`;
+            const previousValue = currentTranslate;
+            const newTranslate = `translateX(-${Number(this.scrollbar.value) * TranslateValue}px)`;
+            const animation = [{ transform: previousValue }, { transform: newTranslate }];
+            currentTranslate = newTranslate;
+            sourcesTrack.style.transform = currentTranslate;
+            sourcesTrack.animate(animation, { duration: 2000 });
+        }
+    }
+
     public draw(data: SourceItem[]) {
         const fragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
@@ -16,6 +39,12 @@ class Sources {
         });
 
         (document.querySelector('.sources') as HTMLElement).append(fragment);
+
+        if (this.scrollbar) {
+            this.scrollbar.addEventListener('input', () => {
+                this.slideSources(data.length);
+            });
+        }
     }
 }
 
