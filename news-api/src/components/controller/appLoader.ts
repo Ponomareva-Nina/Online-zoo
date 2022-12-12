75349a30c49ad8534ccee440e668720c79579d16
Nina Ponomareva <nina.9412@mail.ru>
2022-12-12 18:23:42 +0300

    feat: add configurations for deploy

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '50c95b2697574c248fdb1871f00d7ff7', // получите свой ключ https://newsapi.org/
         });
     }
